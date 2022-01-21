16882c78596e417ee3329662b364ea0db2513980
Ali Ahmed <aliahmedaiub@gmail.com>
2022-01-21 21:43:49 +0600

    Initial commit

---
+++ app/src/main/java/com/bracbank/cookpadrecipies/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.bracbank.cookpadrecipies.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bracbank.cookpadrecipies.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
